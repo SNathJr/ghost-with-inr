57dea223584e2b7ba09d1f968b1074b9a3c7e98c
Swastik Nath <swastik.nath@entropiktech.com>
2020-06-09 18:51:36 +0530

    Ghost: INR support

---
+++ core/server/lib/common/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+    get i18n() {
+        return require('./i18n');
+    },
+
+    get events() {
+        return require('./events');
+    },
+
+    get logging() {
+        return require('./logging');
+    }
+};
